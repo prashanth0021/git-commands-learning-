867f3239505aa4746b4b25b3e7827ebd800a78e5
prashanth0021 <prashanthsajjala33@gmail.com>
2025-12-03 14:24:59 +0530

    Button and alert changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,19 @@
-import ListGroup from "./components/ListGroup";
+import Button  from "./components/Button";  
+import React, { useState } from "react"; 
+import Alert from "./components/alert";
 function App() {
-  let items = ["An item", "A second item", "A third item", "A fourth item", "And a fifth one"];
-  const handleSelection = (item: string) => {
-    console.log(item);
-  };
-  return (
-    <div>
-      <ListGroup items={items} heading="Items" onSelectItem={handleSelection} />
-    </div>
-  );
+  const [alertVisible, setAlertVisible] = useState(false);
+      return(
+        <>
+        {alertVisible && <Alert onClose={()=>setAlertVisible(false)}> This is an alert message!</Alert>}
+        <div>
+         <Button color='primary' onClick={()=>setAlertVisible(true)}>
+            Click Me
+          </Button>
+        </div>
+        </>
+      );
+    
 }
 export default App;
     
